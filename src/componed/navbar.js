ebfa83063ef1670bc7642067eb43456f7945ded4
riadhramdhani <riadh.etoo@gmail.com>
2023-03-06 14:19:48 +0100

    reeee

--- src/componed/navbar.js
+++ src/componed/navbar.js
@@ -11,6 +11,7 @@ import Newproduct from './modal';
 
 
 function navbar() {
+  
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
@@ -24,19 +25,6 @@ function navbar() {
           >
             <Nav.Link href="#action1"></Nav.Link>
             <Nav.Link href="#action2">< Newproduct  /></Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
